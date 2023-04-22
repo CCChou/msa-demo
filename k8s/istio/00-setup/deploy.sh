d909dba652a85c7533319e68aeb341de06640616
dennischou <dses5900@gmail.com>
2023-04-22 22:05:23 +0800

    Add caj istio demo

---
+++ k8s/istio/00-setup/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]
+then 
+  echo "usage: deploy.sh [namespace] [host]" 
+  exit 1
+fi
+
+export NAMESPACE=$1
+export HOST=$2
+envsubst < caj-services.yaml | oc apply -f -
